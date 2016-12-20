d95ab912662db647dee0158b00baf315f3721aeb
Hanzhao <hanzhao1994@vip.qq.com>
2016-12-20 16:18:11 +0800

    Initial Commit

---
+++ UIImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIImagePicker
+//
+//  Created by 韩钊 on 2016/12/20.
+//  Copyright © 2016年 韩钊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIImagePicker
+//
+//  Created by 韩钊 on 2016/12/20.
+//  Copyright © 2016年 韩钊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
